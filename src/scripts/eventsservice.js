c5306201e8d1eef3b82088db00ef02bb9648d8c2
varc00l <ivalchuk4@gmail.com>
2025-03-13 19:52:29 +0200

    add class, pageData andgetEvents

--- src/scripts/eventsservice.js
+++ src/scripts/eventsservice.js
@@ -1,2 +1,37 @@
 const ROOT_URL = 'https://app.ticketmaster.com/discovery/v2';
 const API_KEY = 'XoWdlrAgDbi7pRSRibKrECGRIIYtPAXc';
+
+class EventsService {
+    constructor() {
+    this.events = [];
+    this.keyword = '';
+    this.countryCode = '';
+    this.page = 0;
+    this.size = 20;
+    this.totalPages = 0;
+  }
+
+  getPageData() {
+    return {
+      page: this.page,
+      totalPages: this.totalPages,
+    };
+  }
+
+ getEvents() {
+    const url = new URL(`${ROOT_URL}/events`);
+    url.searchParams.append('apikey', API_KEY);
+    url.searchParams.append('keyword', this.keyword);
+    url.searchParams.append('page', this.page);
+    url.searchParams.append('countryCode', this.countryCode);
+    url.searchParams.append('size', this.size);
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.events = data?._embedded?.events ?? [];
+        this.totalPages = data.page.totalPages;
+        return this.events;
+      });
+    }
+}
